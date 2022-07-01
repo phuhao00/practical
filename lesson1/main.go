93838c0961a9fae16c79dd9c8218adc56f050294
calvin <calvin@taomee.com>
2022-07-01 18:04:20 +0800

    1-4

---
+++ lesson1/main.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+
+}
+
+//StartGameServer  启动游戏服
+func StartGameServer() {
+
+}
+
+//LoadConf 加载配置
+func LoadConf() {
+
+}
+
+//LoadDBData 加载DB 数据
+func LoadDBData() {
+
+}
+
+//ResolveGameLogic 处理游戏逻辑
+func ResolveGameLogic() {
+
+}
+
+//SaveGameData  存储游戏数据
+func SaveGameData() {
+
+}
+
+//StopGameServer 停止游戏服
+func StopGameServer() {
+
+}
+
+//Log 打印日志
+func Log() {
+
+}
