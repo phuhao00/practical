f12a7eedc49af49cdc4f290abb1b2af856b4b22d
calvin <hhaou@outlook.com>
2022-07-03 18:27:23 +0800

    第二讲

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,36 +1,47 @@
 package main
 
 func main() {
-
+	LoadConf()
+	LoadDBData()
+	StartGameServer()
+	ResolveGameLogic()
+	StopGameServer()
 }
 
 //StartGameServer  启动游戏服
 func StartGameServer() {
+	Log()
 
 }
 
 //LoadConf 加载配置
 func LoadConf() {
+	Log()
 
 }
 
 //LoadDBData 加载DB 数据
 func LoadDBData() {
+	Log()
 
 }
 
 //ResolveGameLogic 处理游戏逻辑
 func ResolveGameLogic() {
+	SaveGameData()
+	Log()
 
 }
 
 //SaveGameData  存储游戏数据
 func SaveGameData() {
+	Log()
 
 }
 
 //StopGameServer 停止游戏服
 func StopGameServer() {
+	Log()
 
 }
 
